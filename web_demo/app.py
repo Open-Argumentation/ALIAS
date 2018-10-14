ea80fadcf6510f0d478e38ef58b2d712e23b1ed0
Marcin Szczot <40180425@napier.ac.uk>
2018-10-14 18:20:40 +0100

    completed web demo

--- web_demo/app.py
+++ web_demo/app.py
@@ -8,14 +8,31 @@ app = Flask(__name__, template_folder='templates')
 app.config['STATIC_FOLDER'] = 'static'
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
 
+af = None;
+
 @app.route('/')
 def demo():
     return render_template('index.html')
 
 @app.route('/framework/<af_id>', methods=['POST'])
 def framework(af_id):
+    global af
     af = alias.read_tgf(static_file_dir + '/examples/' + af_id + '.tgf')
     return alias.get_json(af)
+
+@app.route('/extension/<ext_id>', methods=['POST'])
+def extension(ext_id):
+    global af
+    if af is not None:
+        if ext_id == 'Stable':
+            return json.dumps([list(v) for v in af.get_stable_extension()])
+        if ext_id == 'Complete':
+            return json.dumps([list(v) for v in af.get_complete_extension()])
+        if ext_id == 'Preferred':
+            return json.dumps([list(v) for v in af.get_preferred_extension()])
+    else:
+        return "Please select argumentation framework first"
+
 
 
 if __name__ == '__main__':
--- web_demo/static/js/main.js
+++ web_demo/static/js/main.js
@@ -1,38 +1,72 @@
 $(function(){
 
     setupFrameworkList();
-
+    setupExtensions();
+    enableExtensions();
 });
 
+var is_framework = false;
 var frameworks = ['af1', 'af2', 'af3', 'af4', 'af5', 'af6'];
+var extensions = ['Complete', 'Stable', 'Preferred'];
+var selected_extension = '';
+var cy;
 
 function setupFrameworkList() {
     var myList = $('#example_frameworks');
     myList.empty();
 
     $.each(frameworks, function (i) {
-        var li = $('<li/>')
-            .attr('id', frameworks[i])
-            .appendTo(myList);
-
         var aa = $('<a/>')
+            .addClass('dropdown-item')
+            .attr('id', frameworks[i])
             .text(frameworks[i])
-            .appendTo(li);
+            .appendTo(myList);
     });
 
-    $('#example_frameworks li').click(function () {
+    $('#example_frameworks a').click(function () {
+        $('.sol_list').empty();
         $.post('/framework/' + this.id, function (data) {
-            var cy = cytoscape({
-                container: $('#cy'),
-            });
 
             var json = $.parseJSON(data)
-            console.log(json['arguments'])
+            cy = cytoscape({
+                container: $('#cy'),
+                style: [
+                    {
+                        selector: '.node_sol',
+                        style: {
+                            'background-color': 'green',
+                        }
+                    },
+                    {
+                        selector: 'node',
+                        style: {
+                            'label': 'data(label)',
+                            'border': 'grey',
+                            'labelValign': 'center',
+                            'labelHalign': 'center',
+                        }
+                    },
+                    {
+                        selector: 'edge',
+                        style: {
+                            'curve-style': 'bezier',
+                            'width': 3,
+                            'line-color': '#ccc',
+                            'target-arrow-color': '#ccc',
+                            'target-arrow-shape': 'triangle'
+                        }
+                    }
+
+                ]
+            });
 
             $.each(json['arguments'], function(i, obj) {
                 cy.add({
-                    data: {id: obj['name'], label: obj['name']}
-                })
+                    data: {
+                        id: obj['name'],
+                        label: obj['name']
+                    }
+                });
             });
 
             $.each(json['arguments'], function(i, obj) {
@@ -47,9 +81,71 @@ function setupFrameworkList() {
                 })
             });
 
-            cy.layout({name: 'cola',
+            cy.layout({
+                name: 'cola',
                     infinite: true,
-                    fit: false,})
+                    fit: false,
+            });
+
+            is_framework = true;
+            enableExtensions();
+        });
+    });
+}
+
+function enableExtensions()
+{
+    if (is_framework)
+    {
+        $('.ext_element').removeClass('disabled');
+    }
+    else
+    {
+        $('.ext_element').addClass('disabled');
+    }
+}
+
+function setupExtensions()
+{
+    var myList = $('#extensions');
+    $.each(extensions, function (i) {
+        var aa = $('<a/>')
+            .text(extensions[i])
+            .attr('id', extensions[i])
+            .addClass('ext_element')
+            .addClass('dropdown-item')
+            .appendTo(myList);
+    });
+
+    $('#extensions a').click(function () {
+        selected_extension = this.id;
+        $.post('/extension/' + this.id, function (data) {
+            var jsonReturn = $.parseJSON(data);
+
+            $('#sol_title').text('Solution: ' + selected_extension + ' extensions')
+
+            $('.sol_list').empty();
+
+            $.each(jsonReturn, function(i, item) {
+                $('<a/>')
+                    .text(item)
+                    .addClass('list-group-item')
+                    .addClass('list-group-item-action')
+                    .addClass('solution_item')
+                    .attr('id', 'solution' + i)
+                    .appendTo($('.sol_list'));
+            })
+
+            $('.solution_item').click(function()
+            {
+                cy.$('.node_sol').removeClass('node_sol');
+
+                var sol = $('#' + this.id).text().split(',');
+                $.each(sol, function(i, item)
+                {
+                    cy.$('#' + item).addClass('node_sol');
+                })
+            })
         });
     });
 }
